de0d2ca237b512f521dc36ae9c0c7e45dc01ea42
Николай <nikolaymishakin@yandex.ru>
2021-03-21 15:09:27 +0300

    5.3. Построить шахматную доску в псевдографике. [#498341]

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -2,13 +2,13 @@ package ru.job4j.loop;
 
 public class Slash {
     public static void draw(int size) {
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
+        for (int row = 0; row <= size; row++) {
+            for (int cell = 0; cell <= size; cell++) {
                 boolean left = row % 2 == 0 || cell % 2 == 0;
-                //boolean right = ; /* добавить условие, что нужно ставить элемент в правый угол. */
+                boolean right = row % 2 == 0 || cell % 2 == 0; /* добавить условие, что нужно ставить элемент в правый угол. */
                 if (left) {
                     System.out.print("0");
-               // } else if (right) {
+                } else if (right) {
                     System.out.print("0");
                 } else {
                     System.out.print(" ");
