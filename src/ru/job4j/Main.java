dfa8c9c173fd821bdabc0a546d3d764cc6ee840f
Николай <nikolaymishakin@yandex.ru>
2021-03-24 14:25:28 +0300

    6.3. Массив заполнен true или false [#498242]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,25 +1,11 @@
 package ru.job4j;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
-
-    static class WeeklySalary {
-        public static int calculate(int[] hours) {
-            int sum = 0, sum1 = 0, sum3 = 0, sum4 = 0;
-            for (int i = 0; i <= hours.length; i++) {
-                for (int j = 0; j <= i; j++) {
-                    sum += j;
-                        }
-                    }
-            return sum;
-
         }
 
-        public static void main(String[] args) {
-            int[] a = {10, 10, 10, 0, 8, 0, 0};
-            System.out.println(calculate(a));
-        }
-    }
-}
+
+
