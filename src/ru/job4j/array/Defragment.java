776e2f6a16f022b2a6031a3cf33ad23505e153e9
Николай <nikolaymishakin@yandex.ru>
2021-03-26 13:17:48 +0300

    6.8. Дефрагментация массива [#498267]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -1,29 +1,18 @@
 package ru.job4j.array;
-import static ru.job4j.array.SwitchArray.swap;
+
 public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
-            if (array[index] == null) {
-                int point = index;
-                while (point < array.length && array[point] == null) {
-                    point++;
-                }
-                if (point < array.length) {
-                    array[index] = array[point];
-                    array[point] = null;
+            for (int i = 0; i < array.length - index - 1; i++) {
+                if (array[i] == null) {
+                    String notnull = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = notnull;
                 }
             }
+
             System.out.print(array[index] + " ");
         }
         return array;
-    }
-
-    public static void main(String[] args) {
-        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
-        String[] compressed = compress(input);
-        System.out.println();
-        for (int index = 0; index < compressed.length; index++) {
-            System.out.print(compressed[index] + " ");
-        }
     }
 }
---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,13 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+
+        return Arrays.copyOf(rsl, size);
+    }
+}
