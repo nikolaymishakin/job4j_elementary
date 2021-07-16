3ff58dfd3df35d45cf5031ab596587e5509c8f79
Николай <nikolaymishakin@yandex.ru>
2021-07-16 18:03:28 +0300

    Что такое import [#504773]

--- src/ru/job4j/calculator/MathCalculator.java
+++ src/ru/job4j/calculator/MathCalculator.java
@@ -4,17 +4,14 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
+        return sum(first, second) + multiply(first, second);
     }
 
     public static double subtractionAndDivision(double first, double second) {
-        return subtraction(first, second)
-                + division(first, second);
+        return subtraction(first, second) + division(first, second);
     }
 
     public static double sumAndMultAndDivAndSub(double first, double second) {
-        return sumAndMultiply(first, second)
-                + subtractionAndDivision(first, second);
+        return sumAndMultiply(first, second) + subtractionAndDivision(first, second);
     }
 }
